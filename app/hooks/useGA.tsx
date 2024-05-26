c132056c005d93d4ef21eda984ec0ae762e65f0b
Aron Weinraub <pntbtrjs@gmail.com>
2024-05-26 10:42:07 +0300

    ga

--- app/hooks/useGA.tsx
+++ app/hooks/useGA.tsx
@@ -1,6 +1,11 @@
 import ReactGA from "react-ga4";
 
 export const useGa = () => {
-  const sendEvent = (eventName: string, eventObj: {}) => {};
+  const sendEvent = (eventName: string, eventObj: {}) => {
+    ReactGA.send({
+      eventName,
+      ...eventObj,
+    });
+  };
   return { sendEvent };
 };
